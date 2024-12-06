17b353ebfd2b12feff41b6c6a1b555ec81ffea84
shefroyrowe <shefroy.rowe@gmail.com>
2024-12-05 22:54:08 -0800

    add linked list class constructor

--- factory.js
+++ factory.js
@@ -4,3 +4,15 @@ function createNode(value) {
         next: null,
     };
 }
+
+let newNode = createNode('Hello');
+console.log(newNode);
+
+class LinkedList {
+    constructor () {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
