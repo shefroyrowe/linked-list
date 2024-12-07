b54734bb10bdfbfe7c2e64da6397cd889f6e03dd
shefroyrowe <shefroy.rowe@gmail.com>
2024-12-06 16:24:48 -0800

    complete node list

--- factory.js
+++ factory.js
@@ -9,7 +9,7 @@ let newNode = CreateNode('Hello');
 console.log(newNode);
 
 class LinkedList {
-    constructor () {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
@@ -38,7 +38,7 @@ class LinkedList {
         if (this.tail) {
             this.length--;
             const tailNode = this.tail;
-            
+
             // search for the node before tail
             let currentNode = this.head;
             // the while loop stops when the node next
@@ -71,7 +71,7 @@ class LinkedList {
         if (this.head) {
             this.length--;
             const removedNode = this.head;
-            this.head = this.head.ext;
+            this.head = this.head.next;
             return removedNode;
         }
         return undefined;
@@ -83,13 +83,68 @@ class LinkedList {
         if (index === 0) {
             return this.insertHead(value);
         }
+
+        let previousNode = null;
+        let currentNode = this.head;
+
+        // Iterate until we reach the node before the desired index
+        for (let i = 0; i < index; i++) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        // Create the new node
+        const newNode = CreateNode(value);
+
+        // Insert the new node between previous and current
+        if (previousNode) {
+            newNode.next = currentNode;
+            previousNode.next = newNode;
+        } else { // Inserting at the end (previousNode is null)
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return newNode;
+    }
+    removeIndex(index) {
+        if (index >= this.length) {
+            throw new Error("Remove index out of bounds");
+        }
+        if (index === 0) {
+            return this.removeHead();
+        }
+
+        let previousNode = null;
+        let currentNode = this.head;
+
+        // Iterate until we reach the node before the desired index
+        for (let i = 0; i < index; i++) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        // Handle removing the last element (previousNode will be null)
+        if (previousNode === null) {
+            this.head = currentNode.next; // Update head if removing the last element
+            this.tail = null; // Update tail if removing the last element
+        } else {
+            previousNode.next = currentNode.next;
+        }
+
+        this.length--;
+        return currentNode;
     }
 }
 
 const linkedList = new LinkedList();
 
 linkedList.insert(7);
-linkedList.insert(true);
-linkedList.insert(20);
-linkedList.print() // 7 true 20
+linkedList.insert(8);
+linkedList.insert(9);
+linkedList.insert(10);
+linkedList.removeIndex(2); // remove 9
+console.log(linkedList.length); // 3
+linkedList.print(); // 7 8 10
 
